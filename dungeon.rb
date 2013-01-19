3638e8ecb9fd9d2a843040447d9034a65aec76c8
ssjuampi23 <sspablo23@hotmail.com>
2013-01-18 21:10:23 -0600

    Unit Test Dungeon

--- dungeon.rb
+++ dungeon.rb
@@ -33,13 +33,17 @@ class Dungeon
     show_current_description
   end
 
+  #Player
+ 
   class Player
     attr_accessor :name, :location
     def initialize(name)
       @name = name
     end
   end
-
+  
+  #Room
+  
   class Room
     attr_accessor :reference, :name, :description, :connections
     def initialize(reference, name, description, connections)
@@ -53,5 +57,6 @@ class Dungeon
       @name + "\n\nYou are in " + @description
     end
   end
+
 
 end
